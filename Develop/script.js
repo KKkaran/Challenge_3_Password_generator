cbcf8a7ba6c2d2aa5dff4ee3d79d3a719dd19fa0
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2021-11-02 22:25:55 -0400

    getting correct length input plus atleast one criteria set and created lists

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,23 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var lowerCaseCharacters = [
+  'a','b','c','d','e','f','g','h','i','j','k','l','m',
+  'n','o','p','q','r','s','t','u','v','w','x','y','z'
+]
+var upperCaseCharacters = [
+  'A','B','C','D','E','F','G','H','I','J','K','L','M',
+  'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+]
+var numeric = [1,2,3,4,5,6,7,8,9,0]
+var specialCharacters = ['!','"','#','$','%','&','\'','(',')','*','+',',', '-','.'
+                            ,'/',':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~']
 
-// Write password to the #password input
+for(let i=0;i<specialCharacters.length;i++){
+  console.log(specialCharacters[i])
+}
+
+// Write password to the #password input into the box
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,8 +25,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-
 //this function will generate password depending on the criteria
 //and the return value shall be passed to function writePassword
 //which will then display it in the password textArea.
@@ -20,22 +33,45 @@ function generatePassword(){
   //we will provide a couple of prompts and the password shall be created acordingly.
   
   //first we will call a method to get password length
-  //var lengthOfPassword = getPasswordLength();
+  var lengthOfPassword = getPasswordLength();
   //second we will ask if to include lowercase or not
-  var lowerCase = BooleanValue("lowercase");
+  var lowercase;
   //third we wil ask if to include uppercase or not
-  var uppercase = BooleanValue("uppercase");
+  var uppercase;
   //fourth we will ask if to include numeric or not
-  var numeric = BooleanValue("numeric");  
+  var numeric;  
   //fifth we will ask if to include special chars or nor
-  var special = BooleanValue("special");
+  var special;
+  
+  //before we make object we gotta make sure atleast one of the critera is met
+  //if user says no to all of them, we keep asking until they atleast pick one!!
+  var counter = 0;
+  while( !(lowercase === true) && !(uppercase === true) && !(numeric === true) && !(special === true)){
+    //this loop will keep running until user picks atleast one of the criterias
+    if(counter == 0){
+      counter = 1;
+    }else{
+      alert("You have to pick atleast one of the criterias. Let's do it again!")
+    }
+    lowercase = BooleanValue("lowercase");
+    uppercase = BooleanValue("uppercase");
+    numeric = BooleanValue("numeric");
+    special = BooleanValue("special");
+  }
 
   //here once we get all the required criteria we will create an object and pass 
   // to a method to process further creation of password
+  var passwordObject = {
+    count : lengthOfPassword,
+    includeLowerCase : lowercase,
+    includeUpperCase : uppercase,
+    includeNumeric : numeric,
+    includeSpecial : special
+  }
 
-
+  passwordGeneration(passwordObject);
   var passwordText = document.querySelector("#password");
-  passwordText.value =  " " + lowerCase + " " + uppercase + " " + numeric + " " + special;
+  passwordText.value = lengthOfPassword +  " " + lowercase + " " + uppercase + " " + numeric + " " + special;
 
   //return lowerCase;
 }
@@ -63,7 +99,6 @@ function getPasswordLength(){
   return length;
 
 }
-
 //asks user if to include certain criteria in password
 //we use same method for uppercase/lowercase/special/numeric values
 function BooleanValue(criteria){
@@ -77,6 +112,15 @@ function BooleanValue(criteria){
 
 }
 
+//passwordObject including all the password criterias and length will be passed to function to further process it
+function passwordGeneration(passwordObject){
+
+  //console.log(passwordObject);
+  //we have the object which contains the reqd info we need for password generation
+  
+}
+
 // Add event listener to generate button
+
 //generateBtn.addEventListener("click", writePassword);
 generatePassword();
